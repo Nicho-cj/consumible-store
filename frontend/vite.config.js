61a55f60c1820ded111326bd6ea33f2da8d1ec26
Nicho <cruznicholzon@gmail.com>
2026-09-15 12:24:51 -0400

    Añadí dockerfile para desarrollo y producción. Además pequeños cambios para poder ejecutar el proyecto

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server:{
+    host: true   //  Prueba en maquina de nicho
+  },
   test: {
     globals: true,
     environment: 'jsdom',
